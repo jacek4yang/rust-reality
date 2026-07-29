8421531cde21200f5aca3d4a336288686317b1a6
jacek <jacek4yang@outlook.com>
2026-07-29 16:32:45 +0800

    feat: encode VLESS response headers

--- src/protocol/vless/mod.rs
+++ src/protocol/vless/mod.rs
@@ -1,7 +1,11 @@
 mod decode;
 mod read;
+mod response;
 mod types;
 
 pub use decode::{DecodeError, DecodeRequest, decode_request};
 pub use read::{ReadError, ReadRequest, read_request};
+pub use response::{
+    ResponseEncodeError, ResponseWriteError, encode_response_header, write_response_header,
+};
 pub use types::{Address, Command, Destination, RequestHeader, UserId, VERSION};
